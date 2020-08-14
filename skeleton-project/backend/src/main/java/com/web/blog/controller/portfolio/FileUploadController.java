8d8ddf3458e4b1c121946019f7e2f724134f624f
hangji0124 <hangji0124@gmail.com>
2020-08-14 14:09:48 +0900

    200814 | jihye | 다중파일 업로드 기능구현, 정리

--- skeleton-project/backend/src/main/java/com/web/blog/controller/portfolio/FileUploadController.java
+++ skeleton-project/backend/src/main/java/com/web/blog/controller/portfolio/FileUploadController.java
@@ -1,25 +1,19 @@
 package com.web.blog.controller.portfolio;
 
-import java.io.IOException;
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletRequest;
 
 import com.web.blog.model.BasicResponse;
-import com.web.blog.model.portfolio.UploadFile;
+import com.web.blog.model.portfolio.UploadFileRequest;
 import com.web.blog.service.portfolio.FileUploadDownloadService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,23 +22,29 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class FileUploadController {
-    private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+    // private static final Logger logger =
+    // LoggerFactory.getLogger(FileUploadController.class);
 
     @Autowired
     private FileUploadDownloadService service;
 
     @PostMapping("/uploadFile/{pid}")
-    @ApiOperation(value = "폼태그의 파일 업로드 방식으로 파일을 업로드 할 수 있다.")
+    @ApiOperation(value = "폼태그의 파일 업로드 방식으로 파일을 업로드 할 수 있다. - uploadMultipleFiles 완료되면 삭제할거야")
     public ResponseEntity<BasicResponse> uploadFile(@PathVariable int pid, @RequestParam("file") MultipartFile file) {
 
         return service.storeFile(pid, file);
     }
 
-    // 프로젝트별로 파일리스트를 리턴 혹은 사용자별로 파일리스트를 리턴
-    @GetMapping("/uploadFiles")
-    @ApiOperation(value = "업로드 된 파일 리스트를 볼 수 있다.")
-    public Iterable<UploadFile> getUploadFileList() {
-        return service.getFileList();
+    @PostMapping("/uploadMultipleFiles")
+    @ApiOperation(value = "파일들의 배열(list)로 여러 파일을 업로드 할 수 있다. - 이게 파일업로드 api입니다아아아아아아아아아아아아아")
+    public ResponseEntity<BasicResponse> uploadMultipleFiles(@RequestBody UploadFileRequest request) {
+        return service.uploadMultipleFiles(request);
+    }
+
+    @GetMapping("/downloadFile/{fileName}")
+    @ApiOperation(value = "파일 개별 다운로드 - 파일 이름으로 파일을 다운로드 할 수 있다.")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        return service.downloadFile(fileName, request);
     }
 
     @DeleteMapping("/deleteFile/{id}")
@@ -54,43 +54,18 @@ public class FileUploadController {
         return service.deleteFile(id);
     }
 
+    // // 프로젝트별로 파일리스트를 리턴 혹은 사용자별로 파일리스트를 리턴
+    // @GetMapping("/uploadFiles")
+    // @ApiOperation(value = "업로드 된 파일 리스트를 볼 수 있다.")
+    // public Iterable<UploadFile> getUploadFileList() {
+    // return service.getFileList();
+    // }
     // 파일이 없을 경우 없다는 리턴을 해줘야함
-    @GetMapping("/uploadFile/{id}")
-    @ApiOperation(value = "업로드 된 파일의 db내의 id(int)로 파일이 존재하는 지 확인 할 수 있다.")
-    public Optional<UploadFile> getUploadFile(@PathVariable int id) {
-        return service.getUploadFile(id);
-    }
-
-    // @PostMapping("/uploadMultipleFiles")
-    // @ApiOperation(value = "파일들의 배열(list)로 여러 파일을 업로드 할 수 있다.")
-    // public List<UploadFile> uploadMultipleFiles(@RequestParam("files")
-    // MultipartFile[] files) {
-    // return Arrays.asList(files).stream().map(file ->
-    // uploadFile(file)).collect(Collectors.toList());
+    // @GetMapping("/uploadFile/{id}")
+    // @ApiOperation(value = "업로드 된 파일의 db내의 id(int)로 파일이 존재하는 지 확인 할 수 있다.")
+    // public Optional<UploadFile> getUploadFile(@PathVariable int id) {
+    // return service.getUploadFile(id);
     // }
 
     // 파일 다운로드시 한글 깨지는 오류 수정
-    @GetMapping("/downloadFile/{fileName}")
-    @ApiOperation(value = "파일 이름으로 파일을 다운로드 할 수 있다.")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = service.loadFileAsResource(fileName);
-
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename())
-                .body(resource);
-    }
 }
