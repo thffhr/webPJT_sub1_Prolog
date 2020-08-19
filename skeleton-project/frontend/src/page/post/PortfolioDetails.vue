4a8902f5f7c4ea6ce70e528f1b92d64e73ec02f9
박석우 <pseokwooo@gmail.com>
2020-08-19 18:33:23 +0900

    200819 | seokwoo | 파일 전체 압축 다운로드(미완)

--- skeleton-project/frontend/src/page/post/PortfolioDetails.vue
+++ skeleton-project/frontend/src/page/post/PortfolioDetails.vue
@@ -43,9 +43,11 @@
         </b-col>
         <b-col md="6">
           <div style="text-align: right;">
-            <b-button size="sm" variant="outline-dark">
-              <b-icon-cloud-download class="mr-1"></b-icon-cloud-download>전체 파일 다운로드
-            </b-button>
+
+              <b-button @click="downloadAllZip()" size="sm" variant="outline-dark">
+                <b-icon-cloud-download class="mr-1"></b-icon-cloud-download>전체 파일 다운로드
+              </b-button>
+
           </div>
         </b-col>
       </b-row>
@@ -440,6 +442,40 @@ export default {
         console.log(error);
       })
     },
+    downloadAllZip() {
+      axios
+      .get(this.$SERVER_URL + `/downloadPortfolio`, {
+        params: {
+          pid: this.pjtDetail.pid,
+          uid: localStorage["uid"]
+        }
+      }, {
+        responseType: "blob",
+      })
+      .then((response) => {
+        console.log(response)
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        console.log(url)
+        const link = document.createElement('a');
+        console.log(link)
+        link.href = url;
+        link.setAttribute('download', this.pjtDetail.title + ".zip");
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
+    downloadAllZip2() {
+      const url = this.$SERVER_URL + `/downloadPortfolio?pid=${this.pjtDetail.pid}&uid=${localStorage["uid"]}`
+      this.$router.push({ url })
+        // .catch(error => {
+        //   if (error.name != "NavigationDuplicated") {
+        //     throw error;
+        //   }
+        // })
+    },
     fileDownload(id) {
       // console.log(fileName)
       axios
@@ -447,9 +483,10 @@ export default {
         responseType: 'blob',
       })
       .then((response) => {
-        // console.log(response)
+        // console.log("!!!!"+response)
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
+        console.log(link)
         link.href = url;
         link.setAttribute('download', id.fileName);
         document.body.appendChild(link);
